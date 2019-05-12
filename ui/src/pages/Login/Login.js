14d3a40653e23070e9cbfbe9d28c19509c426265
Brian Ramirez <brians@uninorte.edu.co>
2019-05-12 18:52:48 -0500

    refactor: Refactor body and login page css

--- ui/src/pages/Login/Login.js
+++ ui/src/pages/Login/Login.js
@@ -23,19 +23,14 @@ import { saveUser } from '../../actions';
 
 const styles = theme => ({
   main: {
-    width: 'auto',
-    display: 'block', // Fix IE 11 issue.
-    marginLeft: theme.spacing.unit * 3,
-    marginRight: theme.spacing.unit * 3,
-    [theme.breakpoints.up(400 + theme.spacing.unit * 3 * 2)]: {
-      width: 400,
-      marginLeft: 'auto',
-      marginRight: 'auto',
-    },
+    height: '100%',
+    width: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    placeContent: 'center', 
   },
   paper: {
-    marginTop: theme.spacing.unit * 8,
-    marginBottom: theme.spacing.unit * 8,
     display: 'flex',
     flexDirection: 'column',
     alignItems: 'center',
